62030f9237801c8b898e2d45718bf79d9cd2093a
Sahan Hemachandra <45299562+sahanHe@users.noreply.github.com>
2022-07-28 20:55:05 +0530

    Remove build files in git

---
+++ native/src/main/java/io/ballerina/persist/cmd/PersistCmd.java
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package io.ballerina.persist.cmd;
+
+import io.ballerina.cli.BLauncherCmd;
+import io.ballerina.protoc.protobuf.BalGenerationConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+
+import java.io.PrintStream;
+
+
+/**
+ * Class to implement "persist" command for ballerina.
+ */
+@CommandLine.Command(
+        name = "persist",
+        description = "generate Ballerina gRPC client stub for gRPC service for a given gRPC protoc " +
+                "definition.")
+public class PersistCmd implements BLauncherCmd {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PersistCmd.class);
+
+    private static final PrintStream outStream = System.out;
+    private static final String PROTO_EXTENSION = "persist";
+
+    private CommandLine parentCmdParser;
+
+    @CommandLine.Option(names = {"-h", "--help"}, hidden = true)
+    private boolean helpFlag;
+
+    @Override
+    public void execute() {
+        //Help flag check
+        if (helpFlag) {
+            outStream.println("Hello persist");
+            return;
+        }
+    }
+    @Override
+    public void setParentCmdParser(CommandLine parentCmdParser) {
+        this.parentCmdParser = parentCmdParser;
+    }
+    @Override
+    public String getName() {
+        return BalGenerationConstants.COMPONENT_IDENTIFIER;
+    }
+    
+    @Override
+    public void printLongDesc(StringBuilder out) {
+        out.append("Generates ballerina persist client stub for persist service").append(System.lineSeparator());
+        out.append("for a given persistc definition").append(System.lineSeparator());
+        out.append(System.lineSeparator());
+    }
+    
+    @Override
+    public void printUsage(StringBuilder stringBuilder) {
+        stringBuilder.append("  ballerina " + BalGenerationConstants.COMPONENT_IDENTIFIER + " --input chat.grpc\n");
+    }
+}
