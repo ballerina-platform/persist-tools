857a0593673ca3b8a305ecd8a0bd40dfa4069a49
MadhukaHarith <harithmadhu@gmail.com>
2022-08-24 14:05:31 +0530

    Improve test util functions

--- tooling-tests/src/test/java/io/ballerina/persist/tools/ToolingTestUtils.java
+++ tooling-tests/src/test/java/io/ballerina/persist/tools/ToolingTestUtils.java
@@ -32,6 +32,12 @@ import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * persist tool test Utils.
@@ -41,26 +47,36 @@ public class ToolingTestUtils {
     private static final PrintStream errStream = System.err;
 
     public static final String CONFIG_FILE = "Config.toml";
-
-    public static final String REFERENCE_DIRECTORY = "output";
     public static final String GENERATED_SOURCES_DIRECTORY = Paths.get("build", "generated-sources").toString();
-    public static final Path RESOURCE_PATH = Paths.get("src", "test", "resources", "test-src")
+    public static final Path RESOURCES_EXPECTED_OUTPUT = Paths.get("src", "test", "resources", "test-src", "output")
             .toAbsolutePath();
     private static final Path DISTRIBUTION_PATH = Paths.get(".." + File.separator, "target", "ballerina-runtime")
             .toAbsolutePath();
+
     private static ProjectEnvironmentBuilder getEnvironmentBuilder() {
         Environment environment = EnvironmentBuilder.getBuilder().setBallerinaHome(DISTRIBUTION_PATH).build();
         return ProjectEnvironmentBuilder.getBuilder(environment);
     }
 
     public static void assertGeneratedSources(String subDir) {
+        generateSourceCode(Paths.get(GENERATED_SOURCES_DIRECTORY, subDir));
+        Assert.assertTrue(directoryContentEquals(Paths.get(RESOURCES_EXPECTED_OUTPUT.toString()).resolve(subDir),
+                Paths.get(GENERATED_SOURCES_DIRECTORY).resolve(subDir)));
+
+        for (Path actualOutputFile: listFiles(Paths.get(GENERATED_SOURCES_DIRECTORY).resolve(subDir))) {
+            Path expectedOutputFile = Paths.get(RESOURCES_EXPECTED_OUTPUT.toString(), subDir).
+                    resolve(actualOutputFile.subpath(3, actualOutputFile.getNameCount()));
+
+            Assert.assertTrue(Files.exists(actualOutputFile));
+            Assert.assertEquals(readContent(actualOutputFile), readContent(expectedOutputFile));
+        }
+    }
+
+    public static void assertGeneratedSourcesNegative(String subDir) {
         Path sourceDirPath = Paths.get(GENERATED_SOURCES_DIRECTORY, subDir);
-        Path referenceFilePath = Paths.get(RESOURCE_PATH.toString(), REFERENCE_DIRECTORY, subDir);
         Path actualConfigFilePath = sourceDirPath.resolve(CONFIG_FILE);
-        Path referenceConfigFilePath = referenceFilePath.resolve(CONFIG_FILE);
-        generateSourceCode(sourceDirPath.toAbsolutePath());
-        Assert.assertTrue(Files.exists(actualConfigFilePath));
-        Assert.assertEquals(readContent(actualConfigFilePath), readContent(referenceConfigFilePath));
+        generateSourceCode(sourceDirPath);
+        Assert.assertFalse(Files.exists(actualConfigFilePath));
     }
 
     public static void assertAuxiliaryFunctions() {
@@ -91,16 +107,9 @@ public class ToolingTestUtils {
                     "Perform operations on Ballerina Persistent Layer  ballerina persist");
             persistCmdInit.execute();
         } catch (ClassNotFoundException | IllegalAccessException | InstantiationException |
-                 NoSuchMethodException | InvocationTargetException e) {
+                NoSuchMethodException | InvocationTargetException e) {
             errStream.println(e.getMessage());
         }
-    }
-
-    public static void assertGeneratedSourcesNegative(String subDir) {
-        Path sourceDirPath = Paths.get(GENERATED_SOURCES_DIRECTORY, subDir);
-        Path actualConfigFilePath = sourceDirPath.resolve(CONFIG_FILE);
-        generateSourceCode(sourceDirPath);
-        Assert.assertFalse(Files.exists(actualConfigFilePath));
     }
 
     private static void generateSourceCode(Path sourcePath) {
@@ -116,6 +125,48 @@ public class ToolingTestUtils {
             errStream.println(e.getMessage());
         }
     }
+
+    private static List<Path> listFiles(Path path) {
+        Stream<Path> walk = null;
+        try {
+            walk = Files.walk(path);
+        } catch (IOException e) {
+            errStream.println(e.getMessage());
+        }
+        return walk != null ? walk.filter(Files::isRegularFile).collect(Collectors.toList()) : new ArrayList<>();
+    }
+
+    private static boolean directoryContentEquals(Path dir1, Path dir2) {
+        boolean dir1Exists = Files.exists(dir1) && Files.isDirectory(dir1);
+        boolean dir2Exists = Files.exists(dir2) && Files.isDirectory(dir2);
+
+        if (dir1Exists && dir2Exists) {
+            HashMap<Path, Path> dir1Paths = new HashMap<>();
+            HashMap<Path, Path> dir2Paths = new HashMap<>();
+
+            for (Path p : listFiles(dir1)) {
+                dir1Paths.put(dir1.relativize(p), p);
+            }
+
+            for (Path p : listFiles(dir2)) {
+                dir2Paths.put(dir2.relativize(p), p);
+            }
+
+            if (dir1Paths.size() != dir2Paths.size()) {
+                return false;
+            }
+
+            for (Map.Entry<Path, Path> pathEntry : dir1Paths.entrySet()) {
+                Path relativePath = pathEntry.getKey();
+                if (!dir2Paths.containsKey(relativePath)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
     private static String readContent(Path filePath) {
         String content;
         try {
