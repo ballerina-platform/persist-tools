68a199c6d5aa0a71d89e0ec558efb348e0b3a510
Sahan Hemachandra <45299562+sahanHe@users.noreply.github.com>
2022-09-20 03:24:54 +0530

    Add generated sql file to tests

---
+++ tooling-tests/src/test/resources/test-src/output/tool_test_generate_9/target/persist_db_scripts.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS MedicalNeeds;
+CREATE TABLE MedicalNeeds (
+	needId INT NOT NULL AUTO_INCREMENT,
+	itemId INT NOT NULL,
+	beneficiaryId INT NOT NULL,
+	period VARCHAR(191) NOT NULL,
+	urgency VARCHAR(191) NOT NULL,
+	quantity INT NOT NULL,
+	PRIMARY KEY(needId, itemId)
+);
+
+DROP TABLE IF EXISTS MedicalNeeds;
+CREATE TABLE MedicalNeeds (
+	needId INT NOT NULL AUTO_INCREMENT,
+	itemId INT NOT NULL,
+	beneficiaryId INT NOT NULL,
+	period VARCHAR(191) NOT NULL,
+	urgency VARCHAR(191) NOT NULL,
+	quantity INT NOT NULL,
+	PRIMARY KEY(needId, itemId)
+);
