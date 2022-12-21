190cbf899d926479a195a4baea0e9cf761272341
Sahan Hemachandra <45299562+sahanHe@users.noreply.github.com>
2022-12-21 16:44:30 +0530

    Add test cases

--- persist-cli-tests/src/test/java/io/ballerina/persist/tools/ToolingGenerateTest.java
+++ persist-cli-tests/src/test/java/io/ballerina/persist/tools/ToolingGenerateTest.java
@@ -174,4 +174,11 @@ public class ToolingGenerateTest {
     public void testGenerateUpdateClientsWithAlreadyInitializedClients() {
         assertGeneratedSourcesNegative ("tool_test_generate_24", GENERATE, new String[]{});
     }
+
+    @Test(enabled = true)
+    @Description("There are three entities with one to one associations between each other with comments " +
+            "in entity fields")
+    public void testGenerateThreeEntitiesWith1To1AssociationsWithComments() {
+        assertGeneratedSources("tool_test_generate_27", GENERATE);
+    }
 }
