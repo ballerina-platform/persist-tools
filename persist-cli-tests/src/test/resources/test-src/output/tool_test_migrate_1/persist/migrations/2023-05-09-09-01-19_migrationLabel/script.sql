88184ad1260de6a86be48fd93c448474406712c1
Akshay De Silva <akshay@wso2.com>
2023-05-10 18:05:12 +0530

    Update test case to new format

--- persist-cli-tests/src/test/resources/test-src/output/tool_test_migrate_1/persist/migrations/2023-05-09-09-01-19_migrationLabel/script.sql
+++ persist-cli-tests/src/test/resources/test-src/output/tool_test_migrate_1/persist/migrations/2023-05-09-09-01-19_migrationLabel/script.sql
@@ -4,22 +4,32 @@ CREATE TABLE MedicalObject (
     types VARCHAR(191),
     objectFlag BOOLEAN
 );
+
 CREATE TABLE MedicalTest (
     testId VARCHAR(191) PRIMARY KEY,
     object1ObjectId INT
 );
+
 ALTER TABLE MedicalNeed
 ADD COLUMN description VARCHAR(191);
+
 ALTER TABLE MedicalNeed
 DROP PRIMARY KEY;
+
 ALTER TABLE MedicalNeed
 ADD PRIMARY KEY (itemId);
+
 ALTER TABLE MedicalTest
 ADD CONSTRAINT FK_MedicalTest_MedicalObject FOREIGN KEY (object1ObjectId) REFERENCES MedicalObject(objectId);
+
 ALTER TABLE MedicalNeed
 DROP COLUMN beneficiaryId;
+
 DROP TABLE MedicalItem;
+
 ALTER TABLE MedicalNeed
 MODIFY COLUMN needId VARCHAR(191);
+
 ALTER TABLE MedicalNeed
 MODIFY COLUMN itemId VARCHAR(191);
+
