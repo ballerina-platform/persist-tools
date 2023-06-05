347292294b6e78d80b06e50eb3d3cbf54e704b57
Kaneel Dias <kaneeldias@hotmail.com>
2023-06-05 19:31:32 +0530

    Fix failing test cases

--- persist-cli/src/main/java/io/ballerina/persist/nodegenerator/syntax/sources/InMemorySyntaxTree.java
+++ persist-cli/src/main/java/io/ballerina/persist/nodegenerator/syntax/sources/InMemorySyntaxTree.java
@@ -148,7 +148,7 @@ public class InMemorySyntaxTree implements SyntaxTree {
         Function queryOne = new Function(String.format(BalSyntaxConstants.QUERY_ONE, nameInCamelCase),
                 SyntaxKind.OBJECT_METHOD_DEFINITION);
         queryOne.addQualifiers(new String[] { BalSyntaxConstants.KEYWORD_ISOLATED });
-        queryOne.addReturns(TypeDescriptor.getSimpleNameReferenceNode(BalSyntaxConstants.QUERY_ONE_RETURN));
+        queryOne.addReturns(TypeDescriptor.getSimpleNameReferenceNode(BalSyntaxConstants.QUERY_ONE_RETURN_IN_MEMORY));
         queryOne.addRequiredParameter(TypeDescriptor.getSimpleNameReferenceNode("anydata"),
                 BalSyntaxConstants.KEYWORD_KEY);
         queryOne.addStatement(NodeParser.parseStatement(clonedTables));
