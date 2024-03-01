9eb8b7e2dfc7031b8dac51ba74b852c6ea7635a4
Haritha Hasathcharu <harithahasathcharu@icloud.com>
2024-03-01 16:45:09 +0530

    Update test cases to pass code quality

--- persist-cli/src/test/java/io/ballerina/persist/UnitTest.java
+++ persist-cli/src/test/java/io/ballerina/persist/UnitTest.java
@@ -26,74 +26,59 @@ import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 /**
  * A unit test class for singular to plural functions.
  */
 public class UnitTest {
-    List<String> singularInput = Arrays.asList(
+
+    List<String> singularWords = Arrays.asList(
             "boy", "girl", "bird", "cod", "commerce", "quiz", "lemma", "dingo", "echo", "yes",
-            "tornado", "have", "dingo", "bus", "child", "mom", "dad", "bottle", "sticker", "moss",
+            "tornado", "dingo", "bus", "child", "mom", "dad", "bottle", "sticker", "moss",
             "wolf", "wife", "life", "leaf", "woman", "mouse", "goose", "baby", "toy", "kidney",
             "potato", "memo", "stereo", "sheep", "deer", "series", "species", "window", "sticker", "desk",
             "pencil", "cup", "milk", "choice", "box", "thief", "army", "woman", "friend", "daisy",
             "boss", "marsh", "class", "lunch", "belief", "chef", "city", "ray", "photo", "piano",
             "cactus", "focus", "series", "species", "mouse", "foot", "nanny", "study", "fox", "pouch",
-            "brush", "quiz", "roof", "truck", "bug", "pen", "book", "vegetable", "chair", "bacteria",
-            "medium", "bacterium", "kangaroo", "cherry", "sky", "monkey", "berry", "video", "studio", "mango",
-            "tornado", "tuxedo", "volcano", "house", "sister", "item", "thing", "computer", "flower", "roof", "has"
+            "brush", "quiz", "roof", "truck", "bug", "pen", "book", "vegetable", "chair",
+            "medium", "kangaroo", "cherry", "sky", "monkey", "berry", "video", "studio", "mango",
+            "tornado", "tuxedo", "volcano", "house", "sister", "item", "thing", "computer", "flower", "roof",
+            "bacterium", "have"
     );
-    List<String> pluralOutput = Arrays.asList(
+
+    List<String> pluralWords = Arrays.asList(
             "boys", "girls", "birds", "cod", "commerce", "quizzes", "lemmata", "dingoes", "echoes", "yeses",
-            "tornadoes", "have", "dingoes", "buses", "children", "moms", "dads", "bottles", "stickers", "mosses",
+            "tornadoes", "dingoes", "buses", "children", "moms", "dads", "bottles", "stickers", "mosses",
             "wolves", "wives", "lives", "leaves", "women", "mouses", "geese", "babies", "toys", "kidneys",
             "potatoes", "memos", "stereos", "sheep", "deer", "series", "species", "windows", "stickers", "desks",
             "pencils", "cups", "milks", "choices", "boxes", "thieves", "armies", "women", "friends", "daisies",
             "bosses", "marshes", "classes", "lunches", "beliefs", "chefs", "cities", "rays", "photos", "pianos",
             "cactuses", "focuses", "series", "species", "mouses", "feet", "nannies", "studies", "foxes", "pouches",
-            "brushes", "quizzes", "roofs", "trucks", "bugs", "pens", "books", "vegetables", "chairs", "bacteria",
-            "mediums", "bacteria", "kangaroos", "cherries", "skies", "monkeys", "berries", "videos", "studios",
+            "brushes", "quizzes", "roofs", "trucks", "bugs", "pens", "books", "vegetables", "chairs",
+            "mediums", "kangaroos", "cherries", "skies", "monkeys", "berries", "videos", "studios",
             "mangoes", "tornadoes", "tuxedos", "volcanoes", "houses", "sisters", "items", "things", "computers",
-            "flowers", "roofs", "have"
+            "flowers", "roofs", "bacteria", "have"
     );
 
-    List<String> pluralInput = Arrays.asList(
-            "boys", "girls", "birds", "cod", "commerce", "quizzes", "lemmata", "dingoes", "echoes", "yeses",
-            "tornadoes", "have", "dingoes", "buses", "children", "moms", "dads", "bottles", "stickers", "mosses",
-            "wolves", "wives", "lives", "leaves", "women", "mouses", "geese", "babies", "toys", "kidneys",
-            "potatoes", "memos", "stereos", "sheep", "deer", "series", "species", "windows", "stickers", "desks",
-            "pencils", "cups", "milks", "choices", "boxes", "thieves", "armies", "women", "friends", "daisies",
-            "bosses", "marshes", "classes", "lunches", "beliefs", "chefs", "cities", "rays", "photos", "pianos",
-            "cactuses", "focuses", "series", "species", "mouses", "feet", "nannies", "studies", "foxes", "pouches",
-            "brushes", "quizzes", "roofs", "trucks", "bugs", "pens", "books", "vegetables", "chairs", "bacteria",
-            "mediums", "bacteria", "kangaroos", "cherries", "skies", "monkeys", "berries", "videos", "studios",
-            "mangoes", "tornadoes", "tuxedos", "volcanoes", "houses", "sisters", "items", "things", "computers",
-            "flowers", "roofs", "have"
-    );
+    List<String> inputForPluralTest =
+            Stream.concat(singularWords.stream(), Stream.of("bacterium", "has")).toList();
+    List<String> outputForPluralTest =
+            Stream.concat(pluralWords.stream(), Stream.of("bacteria", "have")).toList();
+    List<String> inputForSingularTest = Stream.concat(pluralWords.stream(), Stream.of("bacterium")).toList();
+    List<String> outputForSingularTest = Stream.concat(singularWords.stream(), Stream.of("bacterium")).toList();
 
-    List<String> singularOutput = Arrays.asList(
-            "boy", "girl", "bird", "cod", "commerce", "quiz", "lemma", "dingo", "echo", "yes",
-            "tornado", "have", "dingo", "bus", "child", "mom", "dad", "bottle", "sticker", "moss",
-            "wolf", "wife", "life", "leaf", "woman", "mouse", "goose", "baby", "toy", "kidney",
-            "potato", "memo", "stereo", "sheep", "deer", "series", "species", "window", "sticker", "desk",
-            "pencil", "cup", "milk", "choice", "box", "thief", "army", "woman", "friend", "daisy",
-            "boss", "marsh", "class", "lunch", "belief", "chef", "city", "ray", "photo", "piano",
-            "cactus", "focus", "series", "species", "mouse", "foot", "nanny", "study", "fox", "pouch",
-            "brush", "quiz", "roof", "truck", "bug", "pen", "book", "vegetable", "chair", "bacterium",
-            "medium", "bacterium", "kangaroo", "cherry", "sky", "monkey", "berry", "video", "studio", "mango",
-            "tornado", "tuxedo", "volcano", "house", "sister", "item", "thing", "computer", "flower", "roof", "have"
-    );
     @Test
     public void testSingularToPlural() {
         long startTime = System.nanoTime();
-        List<String> outputs = singularInput.stream().map(Pluralizer::pluralize).toList();
-        assertResults(startTime, outputs, pluralOutput);
+        List<String> outputs = inputForPluralTest.stream().map(Pluralizer::pluralize).toList();
+        assertResults(startTime, outputs, outputForPluralTest);
     }
     @Test(enabled = true)
     public void testPluralToSingular() {
         long startTime = System.nanoTime();
-        List<String> outputs = pluralInput.stream().map(Singularizer::singularize).toList();
-        assertResults(startTime, outputs, singularOutput);
+        List<String> outputs = inputForSingularTest.stream().map(Singularizer::singularize).toList();
+        assertResults(startTime, outputs, outputForSingularTest);
     }
     private void assertResults(long startTime, List<String> outputs, List<String> expectedOutputs) {
         PrintStream print = System.out;
