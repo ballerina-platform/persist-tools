cb3f3cfa9050c44118f823296fdedbcfae0af371
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2025-10-19 18:53:52 +0530

    Update persist-cli/src/main/java/io/ballerina/persist/nodegenerator/syntax/constants/BalSyntaxConstants.java
    Co-authored-by: Danesh Kuruppu <daneshk@users.noreply.github.com>

--- persist-cli/src/main/java/io/ballerina/persist/nodegenerator/syntax/constants/BalSyntaxConstants.java
+++ persist-cli/src/main/java/io/ballerina/persist/nodegenerator/syntax/constants/BalSyntaxConstants.java
@@ -41,7 +41,7 @@ public class BalSyntaxConstants {
     public static final String EMPTY_STRING = "";
     public static final String SPACE = " ";
     public static final String UNDERSCORE = "_";
-    public static final String SINGLE_QUOTE = "'" ;
+    public static final String SINGLE_QUOTE = "'";
     public static final String COMMA_WITH_SPACE = ", ";
     public static final String COLON = ":";
     public static final String SEMICOLON = ";";
